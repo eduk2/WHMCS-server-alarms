7a42a1f9f5466d88f2f29a00d775775e44f608b8
Eduardo <eduardo.jimpen@gmail.com>
2022-03-01 22:09:14 +0000

    Implementation of getDatabaseConnection

--- whmcs/whmcs.go
+++ whmcs/whmcs.go
@@ -1,8 +1,22 @@
 package whmcs
 
-// get database connection from whmcs
+import (
+	v "github.com/eduk2/govarsfromfiles"
+)
 
+// get database connection from whmcs
 func getDatabaseConnection(configurationFilePath string) (dbHost, dbUser, dbPass, dbName string) {
+
+	if v.File.SetPath(configurationFilePath) {
+
+		v.File.SetRegularExp(`'(.*)'`)
+		myValues := v.File.GetValues("$db_host", "$db_username", "$db_name", "$db_password")
+		dbHost = myValues["$db_host"]
+		dbUser = myValues["$db_username"]
+		dbName = myValues["$db_name"]
+		dbPass = myValues["$db_password"]
+
+	}
 
 	return
 }
