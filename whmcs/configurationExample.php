10fc4b689028457db908a421139eb7d8f75fa5b6
Eduardo <eduardo.jimpen@gmail.com>
2022-03-01 22:04:35 +0000

    Configuration example php file

---
+++ whmcs/configurationExample.php
@@ -0,0 +1,12 @@
+<?php
+$license = 'WHMCS-2ce4651f55623ab10a3c';
+$db_host = 'localhost';
+$db_username = 'user1';
+$db_password = 'pass1';
+$db_name = 'name1';
+
+$templates_compiledir = "/var";
+$attachments_dir = "/var";
+$downloads_dir = "/var";
+
+$mysql_charset = 'utf8';
