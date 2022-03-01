cc37dde4887d06c6373224762aafeb90c5fa6e2b
Eduardo <eduardo.jimpen@gmail.com>
2022-03-01 21:26:28 +0000

    Initial comments in main file

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+	// get list of servers of WHMCS database
+
+	// get status code of servers
+
+	// write a json file with list of servers and their status code
+
+	// compare with previous status code list of servers
+
+	// send a notification if the status code changes from up to down
+
+}
