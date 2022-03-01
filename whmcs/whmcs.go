c7356cdd20986e9234e4b0064d77c6097f29c91f
Eduardo <eduardo.jimpen@gmail.com>
2022-03-01 22:05:17 +0000

    Definition of the function to get the database connection

---
+++ whmcs/whmcs.go
@@ -0,0 +1,12 @@
+package whmcs
+
+// get database connection from whmcs
+
+func getDatabaseConnection(configurationFilePath string) (dbHost, dbUser, dbPass, dbName string) {
+
+	return
+}
+
+// connect to whmcs
+
+// get list of servers
